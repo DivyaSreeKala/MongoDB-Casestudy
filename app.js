da33573daab5a5bf73630418e0892f9a45023f12
DivyaSreeKala <divyasreekala99@example.com>
2024-09-29 23:10:25 +0530

    added put and delete

--- app.js
+++ app.js
@@ -3,12 +3,17 @@
 const express = require('express');
 const app = new express();
 
+const methodOverride = require('method-override');
+
+
 const morgan = require('morgan');
 app.use(morgan('dev'));
 
 app.set('view engine','ejs');
 app.set('views',__dirname+'/views')
 
+
+app.use(methodOverride('_method'));
 
 const movieRoutes = require('./router/employeeRoutes');
 app.use('/employee',movieRoutes);
@@ -19,6 +24,8 @@ require('./db/connection')
 require('dotenv').config();//require .env file
 
 PORT = process.env.PORT;
+
+
 
 
 
--- router/employeeRoutes.js
+++ router/employeeRoutes.js
@@ -36,10 +36,16 @@ router.get('/add',(req,res)=>{
 router.post('/add-employee',async (req,res)=>{
     // res.render("home.ejs")
     try{
-        const data = req.body;
-        const employeeModal1 = new employeeModal(data);
-        const savedData = await  employeeModal1.save();
-        res.status(200).send("post successful")
+        const data1 = req.body;
+        const employeeModal1 = new employeeModal(data1);
+        const savedData = await employeeModal1.save();
+        const data = await employeeModal.find();
+        res.status(200).render('AddEmployee',{
+            title: 'Add Employee',
+            nav,
+            data,
+            message: 'added successfully'   
+        })
 
     }catch(err){
         res.status(404).send("post unsuccessful")
@@ -53,7 +59,7 @@ router.get('/list-employee',async (req,res)=>{
         // const employeeModal1 = new employeeModal(data);
         const data = await  employeeModal.find();
         res.status(200).render('home',{
-            title :"Home Page",
+            title :"List Employees",
             nav,
             data
         })
@@ -63,30 +69,57 @@ router.get('/list-employee',async (req,res)=>{
     }
 })
 
-router.put('/edit-employee/:id',async (req,res)=>{
+router.put('/edit-employee/:id?',async (req,res)=>{
     // res.render("home.ejs")
     try{
         const data = req.body;
         const id = req.params.id;
-        const employeeModal1 = new employeeModal(data);
+        // const employeeModal1 = new employeeModal(data);
         // console.log(employeeModal1)
         // console.log(req.body)
-        const savedData = await  employeeModal.findByIdAndUpdate(id,req.body);
-        res.status(200).send("update successful")
+        console.log(id)
+        const data1 = await employeeModal.findById(id,data)
+        // console.log(employeeModal1)//_id also included here which is immutable and shows error
+        const savedData = await  employeeModal.findByIdAndUpdate(id,data, { new: true } );
+        console.log(savedData)
+        // res.status(200).send('update successful')
+        res.status(200).render('UpdateEmployee',{ title:'update' , nav , data1 , message:'update successful', success: true})
 
     }catch(err){
         console.log(err)
         res.status(404).send("update unsuccessful")
     }
 })
+router.get('/edit-emp/:id',async (req,res)=>{
+    // res.render("home.ejs")
+    try{
+        const data = req.body;
+        const id = req.params.id;
+        // const employeeModal1 = new employeeModal();
+        // console.log(employeeModal1)
+        // console.log(req.body)
+        const data1 = await employeeModal.findById(id)
+        // console.log(data1)
+        // const savedData = await  employeeModal.findByIdAndUpdate(id,req.body);
+        res.status(200).render('UpdateEmployee',{
+            title : 'Update',
+            nav,
+            data1
+        })
 
+    }catch(err){
+        console.log(err)
+        res.status(404).send("update render unsuccessful")
+    }
+})
 router.delete('/delete-employee/:id',async (req,res)=>{
     // res.render("home.ejs")
     try{
         // const data = req.body;
         const id = req.params.id;
         const savedData = await  employeeModal.findByIdAndDelete(id);
-        res.status(200).send("delete successful")
+        const data = await employeeModal.find();
+        res.status(200).render('home',{title:'List Employees',nav,data,message: 'Deleted Successfully'})
 
     }catch(err){
         console.log(err)
